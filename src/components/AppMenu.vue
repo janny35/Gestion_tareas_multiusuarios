7b76c6a6e53549f9e462644235951b14f8e69a8b
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-12-06 06:41:28 -0400

    actualizando hint de buscador

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="img/hero.jpg">
         <form class="d-flex tm-search-form">
-            <input class="form-control tm-search-input" type="search" v-model="usuario" placeholder="Buscar usuario..." aria-label="Search">
+            <input class="form-control tm-search-input" type="search" v-model="usuario" placeholder="Buscar usuarios registrados..." aria-label="Search">
             <button class="btn btn-outline-success tm-search-btn" type="button" @click="buscarUsuario">
                 <i class="fas fa-search"></i>
             </button>
